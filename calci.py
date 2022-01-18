daff221f15e36f0f853afa4ee4f70a3c78b9bab9
aasima007 <97961278+aasima007@users.noreply.github.com>
2022-01-18 18:59:17 +0530

    calculator
    it calculates

---
+++ calci.py
@@ -0,0 +1,13 @@
+x=int(input ("enter the first value"))
+print("select your operation \n 1 for addition \n 2 for multiplication \n 3 for divide \n 4 for subraction")
+y=int(input("enter 1 or2 or3or 4"))
+z=int (input("enter the secound value"))
+
+if y==1:
+    print (x+z)
+elif y==2:
+    print(x*y)
+elif y==3:
+    print(x/y)
+elif y==4:
+    print (x-y)
